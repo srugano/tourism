66559b080c1cd890005df01cd8930941c1be00d4
Allan Stockman Rugano <kuristockyrugano@gmail.com>
2020-10-22 17:30:34 +0200

    added formaters

--- tourism/settings/dev.py
+++ tourism/settings/dev.py
@@ -4,12 +4,12 @@ from .base import *
 DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'pe=%#@%tm2h7d-od2n8*f4-(6+bmb1ckrpbsm9dc=l%_7r=ng='
+SECRET_KEY = "pe=%#@%tm2h7d-od2n8*f4-(6+bmb1ckrpbsm9dc=l%_7r=ng="
 
 # SECURITY WARNING: define the correct hosts in production!
-ALLOWED_HOSTS = ['*'] 
+ALLOWED_HOSTS = ["*"]
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 
 try:
