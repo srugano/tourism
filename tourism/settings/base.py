31d6adb68bcf7553ff649e917f090255334d659a
Allan Stockman Rugano <kuristockyrugano@gmail.com>
2020-10-22 15:42:10 +0200

    some titles corrections

--- tourism/settings/base.py
+++ tourism/settings/base.py
@@ -29,7 +29,7 @@ INSTALLED_APPS = [
 
     'wagtail.contrib.forms',
     'wagtail.contrib.redirects',
-    'wagtail.embeds',
+    #'wagtail.embeds',
     'wagtail.sites',
     'wagtail.users',
     'wagtail.snippets',
